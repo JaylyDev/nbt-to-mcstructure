b08161ab020a7850a156e2f7459eabed9ee4b1f6
Jayly <65847850+JaylyDev@users.noreply.github.com>
2024-12-17 20:37:55 +0000

    empty states convert

--- vanilla-mappings/src/BlockTypeConverters/BaseClass.ts
+++ vanilla-mappings/src/BlockTypeConverters/BaseClass.ts
@@ -1,3 +1,14 @@
+import { bedrockBlocks } from "../Data/MinecraftBlocks";
+
 export abstract class BlockTypeConverterBase {
     public abstract convert(id: string, properties: object): Record<string, string | number | boolean>;
+}
+
+export class EmptyBlockTypeConverter extends BlockTypeConverterBase {
+    public convert(id: string, properties: object): {} {
+        const bedrockBlock = bedrockBlocks.data_items.find((item) => item.name === id);
+        if (!bedrockBlock) throw new Error(`Could not find bedrock block for ${id}`);
+        if (bedrockBlock.properties.length > 0) throw new Error(`Expected Block '${id}' has properties, but they are not converted`);
+        return {};
+    }
 }
--- vanilla-mappings/src/index.ts
+++ vanilla-mappings/src/index.ts
@@ -1,68 +1,14 @@
 import * as fs from "fs";
 import * as path from "path";
 import { ButtonTypeConverter } from "./BlockTypeConverters/Button";
-import { BlockTypeConverterBase } from "./BlockTypeConverters/BaseClass";
+import { BlockTypeConverterBase, EmptyBlockTypeConverter } from "./BlockTypeConverters/BaseClass";
 import { blockIdsMap } from "./Mappings/blocks";
 import { DoorTypeConverter } from "./BlockTypeConverters/Door";
-import { FenceTypeConverter } from "./BlockTypeConverters/Fence";
 import { FenceGateTypeConverter } from "./BlockTypeConverters/FenceGate";
 import { CeilingHangingSignTypeConverter } from "./BlockTypeConverters/CeilingHangingSign";
-import { BlockTypeConverter } from "./BlockTypeConverters/Block";
-import { CauldronTypeConverter } from "./BlockTypeConverters/Cauldron";
-import { AirTypeConverter } from "./BlockTypeConverters/Air";
 import { RedstoneWireTypeConverter } from "./BlockTypeConverters/RedstoneWire";
-
-interface JavaBlockDefinition {
-    type: string;
-    properties: {};
-    block_set_type?: string;
-    color?: string;
-}
-
-interface JavaBlockState {
-    default?: true;
-    id: number;
-    properties?: Record<string, string>;
-}
-
-interface JavaBlock {
-    definition: JavaBlockDefinition;
-    properties?: Record<string, string[]>;
-    states: JavaBlockState[];
-}
-
-interface BlockType {
-    properties: Record<string, string[]>;
-}
-
-interface BedrockBlockPropertyValue {
-    value: string | number | boolean;
-}
-
-interface BedrockBlockProperty {
-    name: string;
-    type: "int" | "bool" | "string";
-    values: BedrockBlockPropertyValue[];
-}
-
-interface BedrockDataItemProperty {
-    name: string;
-}
-
-interface BedrockDataItem {
-    name: string;
-    properties: BedrockDataItemProperty[];
-    raw_id: number;
-    serialization_id: number;
-}
-
-interface MojangBlocks {
-    block_properties: BedrockBlockProperty[];
-    data_items: BedrockDataItem[];
-    module_type: "vanilla_data";
-    name: "mojang-block";
-    vanilla_data_type: "block";
-}
+import { javaBlocks, bedrockBlocks, scriptDir } from "./Data/MinecraftBlocks";
+import { JavaBlock, BlockType, MojangBlocks } from "./Data/Schema";
 
 function getJavaBlockTypes(javaBlocks: Record<string, JavaBlock>): Record<string, BlockType> {
     const blockTypes: Record<string, BlockType> = {};
@@ -153,19 +99,83 @@ function assertBlockTypeConverters(converters: Map<string, BlockTypeConverterBas
 
 function main() {
     // Load data files
-    const scriptDir: string = path.join(__dirname, "..");
-    const javaBlocks: Record<string, JavaBlock> = JSON.parse(fs.readFileSync(path.join(scriptDir, "data/java-blocks.json"), "utf-8"));
-    const bedrockBlocks: MojangBlocks = JSON.parse(fs.readFileSync(path.join(scriptDir, "data/bedrock-blocks.json"), "utf-8"));
     const blockTypeConverters = new Map<string, BlockTypeConverterBase>()
-        .set("minecraft:block", new BlockTypeConverter())
         .set("minecraft:button", new ButtonTypeConverter())
         .set("minecraft:door", new DoorTypeConverter())
-        .set("minecraft:fence", new FenceTypeConverter())
         .set("minecraft:fence_gate", new FenceGateTypeConverter())
         .set("minecraft:ceiling_hanging_sign", new CeilingHangingSignTypeConverter())
-        .set("minecraft:cauldron", new CauldronTypeConverter())
-        .set("minecraft:air", new AirTypeConverter())
-        .set("minecraft:redstone_wire", new RedstoneWireTypeConverter());
+        .set("minecraft:redstone_wire", new RedstoneWireTypeConverter())
+        // Following bedrock blocks has zero block properties, skipping.
+        .set("minecraft:fence", new EmptyBlockTypeConverter())
+        .set("minecraft:block", new EmptyBlockTypeConverter())
+        .set("minecraft:air", new EmptyBlockTypeConverter())
+        .set("minecraft:flower", new EmptyBlockTypeConverter())
+        .set("minecraft:amethyst", new EmptyBlockTypeConverter())
+        .set("minecraft:azalea", new EmptyBlockTypeConverter())
+        .set("minecraft:barrier", new EmptyBlockTypeConverter())
+        .set("minecraft:beacon", new EmptyBlockTypeConverter())
+        .set("minecraft:wool_carpet", new EmptyBlockTypeConverter())
+        .set("minecraft:concrete_powder", new EmptyBlockTypeConverter())
+        .set("minecraft:shulker_box", new EmptyBlockTypeConverter())
+        .set("minecraft:stained_glass", new EmptyBlockTypeConverter())
+        .set("minecraft:stained_glass_pane", new EmptyBlockTypeConverter())
+        .set("minecraft:half_transparent", new EmptyBlockTypeConverter())
+        .set("minecraft:coral_plant", new EmptyBlockTypeConverter())
+        .set("minecraft:coral", new EmptyBlockTypeConverter())
+        .set("minecraft:mushroom", new EmptyBlockTypeConverter())
+        .set("minecraft:budding_amethyst", new EmptyBlockTypeConverter())
+        .set("minecraft:cartography_table", new EmptyBlockTypeConverter())
+        .set("minecraft:weathering_copper_full", new EmptyBlockTypeConverter())
+        .set("minecraft:chorus_plant", new EmptyBlockTypeConverter())
+        .set("minecraft:eyeblossom", new EmptyBlockTypeConverter())
+        .set("minecraft:drop_experience", new EmptyBlockTypeConverter())
+        .set("minecraft:conduit", new EmptyBlockTypeConverter())
+        .set("minecraft:weathering_copper_grate", new EmptyBlockTypeConverter())
+        .set("minecraft:crafting_table", new EmptyBlockTypeConverter())
+        .set("minecraft:fungus", new EmptyBlockTypeConverter())
+        .set("minecraft:nylium", new EmptyBlockTypeConverter())
+        .set("minecraft:roots", new EmptyBlockTypeConverter())
+        .set("minecraft:crying_obsidian", new EmptyBlockTypeConverter())
+        .set("minecraft:base_coral_plant", new EmptyBlockTypeConverter())
+        .set("minecraft:redstone_ore", new EmptyBlockTypeConverter())
+        .set("minecraft:dragon_egg", new EmptyBlockTypeConverter())
+        .set("minecraft:enchantment_table", new EmptyBlockTypeConverter())
+        .set("minecraft:end_portal", new EmptyBlockTypeConverter())
+        .set("minecraft:tall_grass", new EmptyBlockTypeConverter())
+        .set("minecraft:fletching_table", new EmptyBlockTypeConverter())
+        .set("minecraft:transparent", new EmptyBlockTypeConverter())
+        .set("minecraft:iron_bars", new EmptyBlockTypeConverter())
+        .set("minecraft:grass", new EmptyBlockTypeConverter())
+        .set("minecraft:colored_falling", new EmptyBlockTypeConverter())
+        .set("minecraft:hanging_roots", new EmptyBlockTypeConverter())
+        .set("minecraft:heavy_core", new EmptyBlockTypeConverter())
+        .set("minecraft:honey", new EmptyBlockTypeConverter())
+        .set("minecraft:ice", new EmptyBlockTypeConverter())
+        .set("minecraft:infested", new EmptyBlockTypeConverter())
+        .set("minecraft:jukebox", new EmptyBlockTypeConverter())
+        .set("minecraft:mangrove_roots", new EmptyBlockTypeConverter())
+        .set("minecraft:bonemealable_feature_placer", new EmptyBlockTypeConverter())
+        .set("minecraft:carpet", new EmptyBlockTypeConverter())
+        .set("minecraft:mud", new EmptyBlockTypeConverter())
+        .set("minecraft:mycelium", new EmptyBlockTypeConverter())
+        .set("minecraft:nether_sprouts", new EmptyBlockTypeConverter())
+        .set("minecraft:netherrack", new EmptyBlockTypeConverter())
+        .set("minecraft:snowy_dirt", new EmptyBlockTypeConverter())
+        .set("minecraft:powder_snow", new EmptyBlockTypeConverter())
+        .set("minecraft:powered", new EmptyBlockTypeConverter())
+        .set("minecraft:redstone_lamp", new EmptyBlockTypeConverter())
+        .set("minecraft:sculk", new EmptyBlockTypeConverter())
+        .set("minecraft:smithing_table", new EmptyBlockTypeConverter())
+        .set("minecraft:snow_layer", new EmptyBlockTypeConverter())
+        .set("minecraft:soul_sand", new EmptyBlockTypeConverter())
+        .set("minecraft:sponge", new EmptyBlockTypeConverter())
+        .set("minecraft:spore_blossom", new EmptyBlockTypeConverter())
+        .set("minecraft:structure_void", new EmptyBlockTypeConverter())
+        .set("minecraft:target", new EmptyBlockTypeConverter())
+        .set("minecraft:tinted_glass", new EmptyBlockTypeConverter())
+        .set("minecraft:waterlogged_transparent", new EmptyBlockTypeConverter())
+        .set("minecraft:wet_sponge", new EmptyBlockTypeConverter())
+        .set("minecraft:wither_rose", new EmptyBlockTypeConverter());
 
     // Assert
     assertBlockTypeConverters(blockTypeConverters, javaBlocks);
